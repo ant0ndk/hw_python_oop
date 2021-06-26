da3b50040980d28e24e236143e3a03215e699fc7
Антон Карпов <antonkarpov@iMac-Anton.local>
2021-06-26 15:31:48 +0300

    1_commit

--- homework.py
+++ homework.py
@@ -2,7 +2,32 @@ import datetime as dt
 
 
 class Calculator:
-    pass
+    def __init__(self, limit):
+        self.limit = limit
+        self.records = []
+        self.today = dt.date.today()
+        self.week_ago = self.today - dt.timedelta(7)
+
+    def add_record(self, record):
+        self.records.append(record)
+
+    def get_today_stats(self):
+        day_stats = []
+        for record in self.records:
+            if record.date == self.today:
+                day_stats.append(record.amount)
+        return sum(day_stats)
+
+    def get_week_stats(self):
+        week_stats = []
+        for record in self.records:
+            if self.week_ago <= record.date <= self.today:
+                week_stats.append(record.amount)
+        return sum(week_stats)
+
+    def get_today_limit_balance(self):
+        limit_balance = self.limit - self.get_today_stats()
+        return limit_balance
 
 
 class Record:
