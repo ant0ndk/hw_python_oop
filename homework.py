dcc31e42475a7a1dbb14847bfb5bcf2b7dc8006b
Anton <kanton2001@yandex.ru>
2021-06-27 14:49:34 +0300

    add CaloriesCalculator

--- homework.py
+++ homework.py
@@ -1,34 +1,52 @@
 import datetime as dt
 
 
+class Record():
+    def __init__(self, amount, comment, date):
+        '''Констркутор класса Record'''
+        self.amount = amount
+        self.comment = comment
+        self.date = dt.date.today()
+
+
 class Calculator:
     def __init__(self, limit):
+        '''Констркутор класса Calculator'''
         self.limit = limit
         self.records = []
         self.today = dt.date.today()
-        self.week_ago = self.today - dt.timedelta(7)
 
-    def add_record(self, record):
-        self.records.append(record)
 
+    def add_record(self, note):
+        '''Добавление записи'''
+        self.record.append(note)
+
+
+    '''Получить статистику на текущий день'''
     def get_today_stats(self):
-        day_stats = []
-        for record in self.records:
-            if record.date == self.today:
-                day_stats.append(record.amount)
-        return sum(day_stats)
+        today_stats = 0
+        for i in self.records:
+            if i.date == dt.date.today():
+                today_stats += i.amount
+        return today_stats
+
 
     def get_week_stats(self):
-        week_stats = []
-        for record in self.records:
-            if self.week_ago <= record.date <= self.today:
-                week_stats.append(record.amount)
-        return sum(week_stats)
-
-    def get_today_limit_balance(self):
-        limit_balance = self.limit - self.get_today_stats()
-        return limit_balance
+        '''Получить статистику на текущую неделю'''
+        week_stats = 0
+        for i in self.records:
+            if i.date <= self.week <= dt.date.today():
+                week_stats += i.amount
+        return week_stats
 
 
-class Record:
-    pass
+class CaloriesCalculator(Calculator):
+    def get_calories_remained(self):
+        eaten_today = self.limit - self.get_today_stats()
+        if eaten_today > 0:
+            msg = (f'Сегодня можно съесть что-нибудь ещё, но с общей '
+                   f'калорийностью не более {eaten_today} кКал')
+        else: 
+            msg = 'Хватит есть!'
+        return msg
+# class CashCalculator(Calculator):
