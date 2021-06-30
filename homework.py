6a339420d5f477ced557cfaf3f192d4ae2b616f3
Anton <kanton2001@yandex.ru>
2021-06-30 13:32:18 +0300

    fix DRY, docstrings, cash_calculator

--- homework.py
+++ homework.py
@@ -2,19 +2,18 @@ import datetime as dt
 
 
 class Record():
-    # Констркутор класса Record
+    '''Констркутор класса Record.'''
     def __init__(self, amount, comment, date=None):
         self.amount = amount
         self.comment = comment
         try:
-            date = dt.datetime.strptime(str(date), '%d.%m.%Y').date()
+            self.date = dt.datetime.strptime(str(date), '%d.%m.%Y').date()
         except Exception:
-            date = dt.date.today()
-        self.date = date
+            self.date = dt.date.today()
 
 
 class Calculator:
-    # Констркутор класса Calculator
+    '''Констркутор класса Calculator.'''
     def __init__(self, limit):
         self.limit = limit
         self.records = []
@@ -34,29 +33,36 @@ class Calculator:
     # Получить статистику на текущую неделю
     def get_week_stats(self):
         week_stats = 0
-        date_now = dt.datetime.today().date()
+        date_now = dt.date.today()
         last_week = date_now - dt.timedelta(days=7)
         for i in self.records:
             if last_week < i.date <= date_now:
                 week_stats += i.amount
         return week_stats
 
+    # Получить остаток на текущий день
+    def get_today_limit(self):
+        sp_today = self.limit - self.get_today_stats()
+        return sp_today
+
 
 class CaloriesCalculator(Calculator):
+    # Получить остаток калорий на текущий день
     def get_calories_remained(self):
-        eaten_today = self.limit - self.get_today_stats()
-        if eaten_today > 0:
+        if self.get_today_limit() > 0:
             return (f'Сегодня можно съесть что-нибудь ещё, но с общей '
-                    f'калорийностью не более {eaten_today} кКал')
+                    f'калорийностью не более {self.get_today_limit()} кКал')
         else:
             return 'Хватит есть!'
 
 
 class CashCalculator(Calculator):
+    # Курсы валют
     USD_RATE = 72.0
     EURO_RATE = 86.0
     RUB_RATE = 1.0
 
+    # Получить остаток денег на текущий день
     def get_today_cash_remained(self, currency):
         currencies = {'usd': (self.USD_RATE, 'USD'),
                       'eur': (self.EURO_RATE, 'Euro'),
@@ -64,16 +70,14 @@ class CashCalculator(Calculator):
         self.cash = self.get_today_stats()
         if self.cash == self.limit:
             return 'Денег нет, держись'
-        try:
-            remained_cash = round((
-                self.limit - self.cash) / currencies[currency][0], 2)
-            debt_cash = round((
-                self.cash - self.limit) / currencies[currency][0], 2)
-        except KeyError:
+        if currency not in currencies:
             return 'Указанной валюты в базе нет'
-        if self.cash < self.limit:
-            return (f'На сегодня осталось {remained_cash}'
-                    f' {currencies[currency][1]}')
+        (rate, name) = currencies[currency]
+        self.cash = round((
+            self.get_today_limit()) / rate, 2)
+        if self.cash > 0:
+            return (f'На сегодня осталось {self.cash}'
+                    f' {name}')
         else:
             return (f'Денег нет, держись: твой долг - '
-                    f'{debt_cash} {currencies[currency][1]}')
+                    f'{abs(self.cash)} {name}')
